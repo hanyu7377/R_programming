43f4fae619e9b239d4cb1404256cadf43f0bb86f
hanyu7377 <80314661+hanyu7377@users.noreply.github.com>
2023-07-13 18:37:46 -0400

    Add files via upload

--- Assignment 2 - Q3.R
+++ Assignment 2 - Q3.R
@@ -2,28 +2,29 @@
 powerSeq <- function(base,maxExp){
   base <- as.integer(base)
   maxExp <- as.integer(maxExp)
-  x <- vector("numeric",maxExp)
-  y <- vector("numeric",maxExp)
-  x[1] <- 0
-  y[1] <- 1
-  for (i in 1:maxExp ){
-    y[i+1] <- base ** i
-    x[i+1] <- i
+  x <- vector("numeric",maxExp + 1)
+  y <- vector("numeric",maxExp + 1)
+  for (i in 0:maxExp ){
+    y[i+1] <- base**i
+    x[i+1] <- i 
   }
-  return (list(x,y))
+  names(y) <- x
+  return (y)
 }
 base_3 <- powerSeq(3,10)
 base_4 <- powerSeq(4,10)
 base_5 <- powerSeq(5,10)
+print(base_3)
+print(base_4)
+print(base_5)
+x_axis <- seq(from = 0,to = 10,by = 1)
+plot(x_axis,base_3, type = "l",col = "green",xlab = "Power", ylab = "value", main = "Power plot")
+lines(x_axis,base_4, type = "l",col = "red")
+lines(x_axis,base_5, type = "l", col= "blue")
+#legend("topleft",legend=c("base 3", "base 4","base 5"),lty=c(1,1,1),col=c("green","red","blue"),bg="white",lwd=2)
 
-plot(unlist(base_3[1]),unlist(base_3[2]), type = "l",col = "green",xlab = "Power", ylab = "value", main = "Power plot")
-lines(unlist(base_4[1]),unlist(base_4[2]), type = "l",col = "red")
-lines(unlist(base_5[1]),unlist(base_5[2]), type = "l", col= "blue")
-legend("topleft",legend=c("base 3", "base 4","base 5"),lty=c(1,1,1),col=c("green","red","blue"),bg="white",lwd=2)
-
-powerChart <- cbind(unlist(base_3[2]),unlist(base_4[2]),unlist(base_5[2]))
+powerChart <- cbind(base_3,base_4,base_5)
 colnames(powerChart) <- c("3","4","5")
-rownames(powerChart) <- paste0( 0:10)
 print(powerChart)
 ############Q3.4   create a powerChartDes matrix
 powerChartDes <- setNames(data.frame(matrix(nrow = 11,ncol = 3)),colnames(powerChart))
@@ -48,16 +49,18 @@ for (i in 1:length(rownames(powerChart))){
 }
 print(negpowerChart)
 ############Q3.6 create C vector 
-C <- vector('numeric', 11)
 for (i in 1:length(rownames(powerChart))){
   result <- 0
   for (j in 1:length(colnames(powerChart))){
-    result <- result + powerChart[i,j]
     if (j == 2){
       result <- result - powerChart[i,j]
+    }
+    else {
+      result <- result + powerChart[i,j]
     }
   }
   C[i] <- result
 }
-
 print(C)
+lines(x_axis,C, type = "l", col= "black")
+legend("topleft",legend=c("base 3", "base 4","base 5","C"),lty=c(1,1,1,1),col=c("green","red","blue","black"),bg="white",lwd=2)
